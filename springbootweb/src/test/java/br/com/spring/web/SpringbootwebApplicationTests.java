e1e43e3b2a96b2e53990743b62914b6d34fe0fe2
Luisa Purificacao <luisa.purificacao@ibm.com>
2022-10-26 15:07:22 -0300

    Spring boot Web Quick Start

---
+++ springbootweb/src/test/java/br/com/spring/web/SpringbootwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
